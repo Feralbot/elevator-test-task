6c38a367b8f0529e81ad82bb488b883a3d57dc80
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-03-20 21:00:03 +0300

    elevator moves

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <main>
-    <div class="elevator"><div class="elevator-cabine"></div></div>
+    <div class="elevator">
+      <div
+        class="elevator-cabine"
+        :style="{ bottom: `${elevatorHeight}vh` }"
+      ></div>
+    </div>
     <div class="buttons">
       <div class="flours">
         <div class="flour" v-for="flour in flours" :key="flour">
-          <button>{{ flour }}</button>
+          <button
+            @click="currentFlour = flour"
+            :class="{ oneFlourUp: currentFlour }"
+          >
+            {{ flour }}
+          </button>
+          {{ currentFlour }}
         </div>
       </div>
     </div>
@@ -12,8 +23,14 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 const flours = ref([1, 2, 3, 4, 5]);
+const currentFlour = ref("");
+const elevatorHeight = ref(0);
+watch(currentFlour, () => {
+  elevatorHeight.value = currentFlour.value * 20-20;
+});
 </script>
 <style src="./styles/reset.css"></style>
 <style src="./styles/main.css"></style>
+<style src="./styles/animations.css"></style>
