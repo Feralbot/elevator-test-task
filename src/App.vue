2730459d9246563349f9c06b6cccb5c21f123d70
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-03-20 22:02:51 +0300

    adding gsap animation

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,7 @@
     <div class="buttons">
       <div class="flours">
         <div class="flour" v-for="flour in flours" :key="flour">
-          <button
-            @click="currentFlour = flour"
-            :class="{ oneFlourUp: currentFlour }"
-          >
+          <button class="flour-btn" @click="currentFlour = flour">
             {{ flour }}
           </button>
           {{ currentFlour }}
@@ -24,12 +21,37 @@
 
 <script setup>
 import { ref, watch } from "vue";
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(
+  Flip,
+  ScrollTrigger,
+  Observer,
+  ScrollToPlugin,
+  Draggable,
+  EaselPlugin,
+  MotionPathPlugin,
+  PixiPlugin,
+  TextPlugin
+);
+
 const flours = ref([1, 2, 3, 4, 5]);
 const currentFlour = ref("");
 const elevatorHeight = ref(0);
-watch(currentFlour, () => {
-  elevatorHeight.value = currentFlour.value * 20-20;
+watch(currentFlour, (flour) => {
+  gsap.to(".elevator-cabine", { y: -160 * (flour - 1), duration: 1 });
 });
+
+// elevatorHeight.value = currentFlour.value * 20 - 20;
 </script>
 <style src="./styles/reset.css"></style>
 <style src="./styles/main.css"></style>
